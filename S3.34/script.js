caea8a9236ccbf40aaf4b3af34c3a458260191b9
unknown <emadeldeen.lina@gmail.com>
2023-04-23 09:38:05 +0300

    23rd Apr

---
+++ S3.34/script.js
@@ -0,0 +1,19 @@
+'use strict';
+console.log(calcAge1(2003));
+console.log(calcAge1(1989));
+
+function calcAge1(birthYear) {
+    const age = 2023 - birthYear;
+    return age;
+}
+
+
+//const age2 = calcAge2(2003);
+
+const calcAge2 = 
+function (birthYear){
+    return 2023 - birthYear;
+}
+
+
+//console.log(calcAge2(2003));
---
+++ S3.35/script.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const age3 = birthYear => 2023 - birthYear;
+console.log(age3(2003));
+
+const yearsUntilRet = (birthYear, firstName) => {
+    const age = 2023 - birthYear;
+    const ret = 60 - age;
+    return `${firstName}'s retirment is in ${ret}`;
+}
+
+console.log(yearsUntilRet(2003,"Leena"));
